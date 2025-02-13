1f188494ba93813debb273f326a6c377446ec1a6
TrinhChanPhuc <trinhchanphuc123@gmail.com>
2025-02-13 10:50:26 +0700

    comment (still working)

--- app/src/main/java/com/example/myapplication/Adapters/Adapter_All_Comments.java
+++ app/src/main/java/com/example/myapplication/Adapters/Adapter_All_Comments.java
@@ -1,4 +1,50 @@
 package com.example.myapplication.Adapters;
 
-public class Adapter_All_Comments {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.Models.ResComment;
+import com.example.myapplication.R;
+
+import java.util.ArrayList;
+
+public class Adapter_All_Comments extends RecyclerView.Adapter<Adapter_All_Comments.Viewholder> {
+
+    private final Context c;
+    private ArrayList<ResComment> commentList;
+
+    public Adapter_All_Comments(Context c, ArrayList<ResComment> commentList) {
+        this.c = c;
+        this.commentList = commentList;
+    }
+
+    @NonNull
+    @Override
+    public Adapter_All_Comments.Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inf = LayoutInflater.from(parent.getContext());
+        View v = inf.inflate(R.layout.rv_product_comment_section, parent, false);
+        return new Viewholder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Viewholder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class Viewholder extends RecyclerView.ViewHolder {
+
+        public Viewholder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
